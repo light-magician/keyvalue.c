935c39cb5d3779323a2b2c3efd8ca8b0d183b487
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2025-01-26 12:04:26 -0500

    refactor: altered tcp server header for generic client handling behavior

--- src/tcp_server.h
+++ src/tcp_server.h
@@ -12,6 +12,7 @@
 //  like getaddrinfo(), gethostbyname(), and
 //  protocol independent name resolution
 #include <netdb.h>
+#include <pthread.h>
 
 /*
  * address info settings
@@ -19,6 +20,10 @@
  * socket file descriptor
  * port number
  * the number of connections the server will accept
+ * a function pointer for handling generic client reqs
+ *  that syntax is a void pointer to a request handler function
+ *  that has a void pointer as a return type for flexible
+ *  returning behavior
  */
 typedef struct {
   struct addrinfo hints;
@@ -26,6 +31,7 @@ typedef struct {
   int socket_fd;
   int port;
   int num_connections;
+  void *(*request_handler)(void *client_connection);
 } TCPServer;
 
 /*
@@ -34,17 +40,28 @@ typedef struct {
  *   of various types IPv4 and IPv6
  *   including the largest socket address types
  * length of a stocket address structure
+ * context is an optional parameter that gets passed
+ *  to each client handler thread, which allows sharing
+ *  data for whatever structures are downstream of the
+ *  TCP server (think like whatever facilitates
+ *    or whatever else gets done later on)
  */
 typedef struct {
   int client_fd;
   struct sockaddr_storage addr;
   socklen_t addr_len;
+  void *context;
 } ClientConnection;
 
-ClientConnection *accept_client_connection(TCPServer *server);
-int handle_client_request(ClientConnection *client);
-int bind_socket_to_addr(TCPServer *server);
-int tcp_server_init(TCPServer *server, int port, int num_connections);
+// core server operations
+int tcp_server_init(TCPServer *server, int port, int num_connections,
+                    void *(*request_handler)(void *), void *context);
 void tcp_server_cleanup(TCPServer *server);
+// socker operations
+int bind_socket_to_addr(TCPServer *server);
+ClientConnection *accept_client_connection(TCPServer *server);
+// client handling
+int handle_client_request(ClientConnection *client);
+void cleanup_client_connection(ClientConnection *client);
 
 #endif
